f57a877bab86550710263a916300352f27f7aca0
hsxazj <2279538834@qq.com>
2024-05-20 13:55:03 +0800

    对开源项目进行初步修改

---
+++ paint/src/cn/edu/nwafu/shape/Images.java
@@ -0,0 +1,17 @@
+package cn.edu.nwafu.shape;
+
+import java.awt.*;
+
+/**
+ *  
+ */
+public class Images extends AbstractShape {
+
+
+    @Override
+    public void draw(Graphics2D g) {
+        g.drawImage(image, 0, 0, board);
+    }
+
+
+}
